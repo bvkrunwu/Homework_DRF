9719d3fe6ffbc53558a3fbf882b99b01b517402f
Alexey Serdiuk <ytfrpkkpreim@mail.ru>
2026-04-10 00:41:31 +0300

    CRUD implementation: Courses managed via ViewSets, Lessons handled with Generic classes, serializers included

--- lms/urls.py
+++ lms/urls.py
@@ -1,3 +1,26 @@
 from django.urls import path
+from rest_framework.routers import DefaultRouter
 
-urlpatterns = []
+from lms.apps import LmsConfig
+from lms.views import (
+    CourseViewSet,
+    LessonCreateApiView,
+    LessonDestroyApiView,
+    LessonListApiView,
+    LessonRetrieveApiView,
+    LessonUpdateApiView,
+)
+
+app_name = LmsConfig.name
+
+
+router = DefaultRouter()
+router.register(r"courses", CourseViewSet, basename="courses")
+
+urlpatterns = [
+    path("lessons/create", LessonCreateApiView.as_view(), name="lesson-create"),
+    path("lessons/", LessonListApiView.as_view(), name="lesson-list"),
+    path("lessons/<int:pk>/", LessonRetrieveApiView.as_view(), name="lesson-detail"),
+    path("lessons/update/<int:pk>/", LessonUpdateApiView.as_view(), name="lesson-update"),
+    path("lessons/delete/<int:pk>/", LessonDestroyApiView.as_view(), name="lesson-delete"),
+] + router.urls
--- lms/views.py
+++ lms/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework import generics, viewsets
 
-# Create your views here.
+from lms.models import Course, Lesson
+from lms.serializers import CourseSerializer, LessonSerializer
+
+
+class CourseViewSet(viewsets.ModelViewSet):
+    serializer_class = CourseSerializer
+    queryset = Course.objects.all()
+
+
+class LessonCreateApiView(generics.CreateAPIView):
+    serializer_class = LessonSerializer
+
+
+class LessonListApiView(generics.ListAPIView):
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+
+
+class LessonRetrieveApiView(generics.RetrieveAPIView):
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+
+
+class LessonUpdateApiView(generics.UpdateAPIView):
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+
+
+class LessonDestroyApiView(generics.DestroyAPIView):
+    queryset = Lesson.objects.all()
