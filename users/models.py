066943e2d6309173a660d87777d14ba0dea05201
Alexey Serdiuk <ytfrpkkpreim@mail.ru>
2026-04-12 13:45:38 +0300

    Add Payment model to users app

--- users/models.py
+++ users/models.py
@@ -1,5 +1,9 @@
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.core.exceptions import ValidationError
 from django.db import models
+
+from config import settings
+from lms.models import Course, Lesson
 
 
 class UserCustomManager(BaseUserManager):
@@ -58,3 +62,33 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.email
+
+
+class Payment(models.Model):
+    PAYMENT_METHOD_CHOICES = [
+        ("cash", "Наличные"),
+        ("transfer", "Перевод на счёт"),
+    ]
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name="Пользователь")
+    payment_date = models.DateTimeField(auto_now_add=True, verbose_name="Дата оплаты")
+    course = models.ForeignKey(
+        Course, on_delete=models.SET_NULL, blank=True, null=True, verbose_name="Оплаченный курс"
+    )
+    lesson = models.ForeignKey(
+        Lesson, on_delete=models.SET_NULL, blank=True, null=True, verbose_name="Оплаченный урок"
+    )
+    amount = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Сумма оплаты")
+    payment_method = models.CharField(max_length=10, choices=PAYMENT_METHOD_CHOICES, verbose_name="Способ оплаты")
+
+    def __str__(self):
+        return f"Платеж от {self.user} на сумму {self.amount} ({self.get_payment_method_display()})"
+
+    class Meta:
+        verbose_name = "Платеж"
+        verbose_name_plural = "Платежи"
+
+    def clean(self):
+        if not self.course and not self.lesson:
+            raise ValidationError("Должен быть указан либо курс, либо урок.")
+        if self.course and self.lesson:
+            raise ValidationError("Нельзя оплатить и курс и, урок одновременно.")
